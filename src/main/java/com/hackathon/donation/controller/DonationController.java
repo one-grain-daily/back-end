9dd50d2d1ccf99926b468aa2b11f028ad460021c
ysndy <ysndy@naver.com>
2023-08-21 15:35:32 +0900

    feat:#1 기부 목록 페이지, 컨트롤러 구현

--- src/main/java/com/hackathon/donation/controller/DonationController.java
+++ src/main/java/com/hackathon/donation/controller/DonationController.java
@@ -1,16 +1,20 @@
 package com.hackathon.donation.controller;
 
 import com.hackathon.donation.domain.Basket;
+import com.hackathon.donation.domain.Donation;
 import com.hackathon.donation.service.DonationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
-@RequestMapping("/donation")
+@RequestMapping("/donations")
 @RequiredArgsConstructor
 public class DonationController {
 
@@ -19,6 +23,17 @@ public class DonationController {
     @GetMapping("/basket")
     public ResponseEntity<Basket> getBasket(){
         return new ResponseEntity<>(donationService.getBasket(), HttpStatus.OK);
+    }
+
+    @GetMapping
+    public String donationList(
+            Model model
+    ){
+
+        List<Donation> donations = donationService.getDonations();
+        model.addAttribute("donations", donations);
+        return "donationList";
+
     }
 
 }
